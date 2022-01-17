8eca83568a3d67c02587e837216dca259d07b31d
david <zrwbgy10@fdbatt.com>
2022-01-17 10:07:56 +0800

    更新EmbedXrpc版本号

--- PublishVersion/Runtime/Cpp/EmbedXrpcObject.h
+++ PublishVersion/Runtime/Cpp/EmbedXrpcObject.h
@@ -4,7 +4,7 @@
 #include "EmbedLibrary.h"
 #include "EmbedXrpcCommon.h"
 #include "BlockBufferProvider.h"
-#define EmbedXrpcObjectVersion	"2.3.4"
+#define EmbedXrpcObjectVersion	"2.4.0"
 
 struct EmbedXrpcBufferConfig
 {
